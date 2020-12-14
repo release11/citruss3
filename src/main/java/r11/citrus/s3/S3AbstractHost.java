36a4f7166f0de05834477182225317fefec91c53
gmod11 <gabriel.modzelewski@release11.com>
2020-12-14 17:03:55 +0100

    Added javadoc

--- src/main/java/r11/citrus/s3/S3AbstractHost.java
+++ src/main/java/r11/citrus/s3/S3AbstractHost.java
@@ -11,11 +11,18 @@ import java.io.*;
 import java.net.URI;
 import java.util.Properties;
 
+/**
+ * Class manages local S3 simulation. It's responsible for both frontend and backend of service.
+ */
 public class S3AbstractHost {
     private final BlobStoreContext context;
     private final BlobStore blobStore;
     private final S3Proxy server;
 
+    /**
+     * Constructor Uses builder configuration to build an object
+     * @param builder
+     */
     public S3AbstractHost(S3AbstractHostBuilder builder){
         Properties properties = new Properties();
         properties.setProperty("jclouds.filesystem.basedir", builder.getLocalBlobPath());
@@ -31,6 +38,10 @@ public class S3AbstractHost {
                 .build();
     }
 
+    /**
+     * Starts and publishes restful server
+     * @throws Exception
+     */
     public void start() throws Exception {
         this.server.start();
         while (!this.server.getState().equals(AbstractLifeCycle.STARTED)) {
@@ -38,38 +49,78 @@ public class S3AbstractHost {
         }
     }
 
+    /**
+     * Shutdowns server
+     * @throws Exception
+     */
     public void stop() throws Exception {
         this.server.stop();
     }
 
+    /**
+     * Static method for obtaining the builder
+     * @return
+     */
     public static S3AbstractHostBuilder builder(){
         return new S3AbstractHostBuilder();
     }
 
+    /**
+     * context getter
+     * @return
+     */
     public BlobStoreContext getContext() {
         return context;
     }
 
+    /**
+     * blobStore getter
+     * @return
+     */
     public BlobStore getBlobStore() {
         return blobStore;
     }
 
+    /**
+     * server getter
+     * @return
+     */
     public S3Proxy getServer() {
         return server;
     }
 
+    /**
+     * Creates a bucket of a given name. Location equal null indicates use of location
+     * (knowns as region, provider etc.) that object has been built with.
+     * @param bucket
+     */
     public void createBucket(String bucket){
         this.blobStore.createContainerInLocation(null, bucket);
     }
 
+    /**
+     * Deletes a bucket of a given name.
+     * @param bucket
+     */
     public void deleteBucket(String bucket){
         this.blobStore.deleteContainer(bucket);
     }
 
+    /**
+     * Deletes all objects in a bucket of a given name.
+     * @param bucket
+     */
     public void clearBucket(String bucket){
         this.blobStore.clearContainer(bucket);
     }
 
+    /**
+     * Creates an object inside a given bucket.
+     * @param bucket
+     * @param key
+     * @param object
+     * @throws IOException
+     */
     public void createObject(String bucket, String key, Object object) throws IOException {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         ObjectOutputStream oos = new ObjectOutputStream(baos);
@@ -81,8 +132,13 @@ public class S3AbstractHost {
         this.blobStore.putBlob(bucket, blob);
     }
 
+    /**
+     * Deletes an object inside a given bucket
+     * @param bucket
+     * @param key
+     */
     public void deleteObject(String bucket, String key){
         this.blobStore.removeBlob(bucket, key);
     }
-    
+
 }
--- src/main/java/r11/citrus/s3/S3AbstractHostBuilder.java
+++ src/main/java/r11/citrus/s3/S3AbstractHostBuilder.java
@@ -1,67 +1,133 @@
 package r11.citrus.s3;
 
+/**
+ * The instance of this class serves as builder to S3AbstractHost. It gathers all required
+ * data to initialize an instance of the class. Operation build returns an object.
+ */
 public class S3AbstractHostBuilder {
+    /** initialized with default value */
     private String service = "filesystem";
+    /** initialized with default value */
     private String region = "eu-east-1";
+    /** initialized with default value */
     private String localBlobPath = "/tmp/blobstore";
+    /** initialized with default value */
     private String host = "http://127.0.0.1:4566";
     private String accessKey;
     private String secretKey;
 
+    /**
+     * service getter
+     * @return
+     */
     public String getService() {
         return service;
     }
 
+    /**
+     * region getter
+     * @return
+     */
     public String getRegion() {
         return region;
     }
 
+    /**
+     * localBlobPath getter
+     * @return
+     */
     public String getLocalBlobPath() {
         return localBlobPath;
     }
 
+    /**
+     * host getter
+     * @return
+     */
     public String getHost() {
         return host;
     }
 
+    /**
+     * accessKey getter
+     * @return
+     */
     public String getAccessKey() {
         return accessKey;
     }
 
+    /**
+     * secretKey getter
+     * @return
+     */
     public String getSecretKey() {
         return secretKey;
     }
 
+    /**
+     * Sets value during building
+     * @param service
+     * @return
+     */
     public S3AbstractHostBuilder service(String service){
         this.service = service;
         return this;
     }
 
+    /**
+     * Sets value during building
+     * @param region
+     * @return
+     */
     public S3AbstractHostBuilder region(String region){
         this.region=region;
         return this;
     }
 
+    /**
+     * Sets value during building
+     * @param localBlobPath
+     * @return
+     */
     public S3AbstractHostBuilder localBlobPath(String localBlobPath){
         this.localBlobPath = localBlobPath;
         return this;
     }
 
+    /**
+     * Sets value during building
+     * @param host
+     * @return
+     */
     public S3AbstractHostBuilder host(String host){
         this.host = host;
         return this;
     }
 
+    /**
+     * Sets value during building
+     * @param accessKey
+     * @return
+     */
     public S3AbstractHostBuilder accessKey(String accessKey){
         this.accessKey = accessKey;
         return this;
     }
 
+    /**
+     * Sets value during building
+     * @param secretKey
+     * @return
+     */
     public S3AbstractHostBuilder secretKey(String secretKey){
         this.secretKey = secretKey;
         return this;
     }
 
+    /**
+     * Creates S3AbstractHost instance based on provided data.
+     * @return
+     */
     public S3AbstractHost build(){
         return new S3AbstractHost(this);
     }
