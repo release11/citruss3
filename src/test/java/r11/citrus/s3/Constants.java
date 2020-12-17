d0190e37d8988f14211d7ff6c3d90834b54da0c7
gmod11 <gabriel.modzelewski@release11.com>
2020-12-17 11:56:53 +0100

    Added properties dependency to test configuration

---
+++ src/test/java/r11/citrus/s3/Constants.java
@@ -0,0 +1,12 @@
+package r11.citrus.s3;
+
+public class Constants {
+    public static final String URI_OVERRIDE = "test.citrus.s3.uri.override";
+    public static final String REGION = "test.citrus.s3.region";
+    public static final String ACCESS_KEY = "test.citrus.s3.access.key";
+    public static final String SECRET_KEY = "test.citrus.s3.secret.key";
+    public static final String LOCAL_BLOBSTORE = "test.citrus.s3.local.blobstore";
+    public static final String SERVICE = "test.citrus.s3.service";
+
+    public static final String TEST_PROPERTIES_FILE = "test.properties";
+}
--- src/test/java/r11/citrus/s3/EndpointConfig.java
+++ src/test/java/r11/citrus/s3/EndpointConfig.java
@@ -1,29 +1,46 @@
 package r11.citrus.s3;
 
+import com.google.common.io.Resources;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 @Configuration
 public class EndpointConfig {
     @Bean
-    public S3AbstractHost s3AbstractHost(){
+    public Properties testProperties(){
+        Properties properties = new Properties();
+        try (InputStream is = Resources.asByteSource(Resources.getResource(
+                Constants.TEST_PROPERTIES_FILE)).openStream()) {
+            properties.load(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return properties;
+    }
+
+    @Bean
+    public S3AbstractHost s3AbstractHost(Properties testProperties){
         return S3AbstractHost.builder()
-                .service("filesystem")
-                .region("eu-north-1")
-                .host("http://localhost:8001")
-                .accessKey("1234")
-                .secretKey("1234")
-                .localBlobPath("./s3-tmp/blobstore")
+                .service(testProperties.getProperty(Constants.SERVICE))
+                .region(testProperties.getProperty(Constants.REGION))
+                .host(testProperties.getProperty(Constants.URI_OVERRIDE))
+                .accessKey(testProperties.getProperty(Constants.ACCESS_KEY))
+                .secretKey(testProperties.getProperty(Constants.SECRET_KEY))
+                .localBlobPath(testProperties.getProperty(Constants.LOCAL_BLOBSTORE))
                 .build();
     }
 
     @Bean
-    public S3Endpoint s3Endpoint() {
+    public S3Endpoint s3Endpoint(Properties testProperties) {
         return S3Endpoint.builder()
-                .endpointUri("http://localhost:8001")
-                .region("eu-north-1")
-                .accessKey("1234")
-                .secretKey("1234")
+                .endpointUri(testProperties.getProperty(Constants.URI_OVERRIDE))
+                .region(testProperties.getProperty(Constants.REGION))
+                .accessKey(testProperties.getProperty(Constants.ACCESS_KEY))
+                .secretKey(testProperties.getProperty(Constants.SECRET_KEY))
                 .build();
     }
 }
